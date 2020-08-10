37d6e074db03e3d29e4cf1f1d4c284561191e48f
Yen-Chen Chou <intelchou@gmail.com>
2020-08-09 19:02:04 -0500

    fix typo

--- src/R/risk_score.R
+++ src/R/risk_score.R
@@ -378,9 +378,9 @@ poi_area <- read_csv('data/processed/RMDS_poi_area_square_feet.csv', col_types =
 open_hours <- read_csv('data/processed/RMDS_open_hours.csv', col_types = cols())
 case_death_table <- read_csv('data/external/LA_County_Covid19_CSA_case_death_table.csv', col_types = cols())
 testing_table <- read_csv('data/external/LA_County_Covid19_CSA_testing_table.csv', col_types = cols())
-file_1_clean <- read_csv('data/processed/patterns-1.csv', col_types = cols())
-file_2_clean <- read_csv('data/processed/patterns-2.csv', col_types = cols())
-file_3_clean <- read_csv('data/processed/patterns-3.csv', col_types = cols())
+file_1_clean <- read_csv('data/processed/pattern-0722.csv', col_types = cols())
+file_2_clean <- read_csv('data/processed/pattern-0729.csv', col_types = cols())
+file_3_clean <- read_csv('data/processed/pattern-0805.csv', col_types = cols())
 print('finish loading required data')
 
 # calculate risk score ----------------------------------------------------
--- src/python/test.py
+++ src/python/test.py
@@ -9,7 +9,7 @@ os.chdir("/Users/yenchenchou/Documents/RMDS_YC/RiskScore/RMDS_COVID19_riskgenera
 
 # %%
 df_pattern = pd.read_csv(
-    "data/external/weekly_pattern/0729/patterns-part1.csv.gz", 
+    "data/external/weekly_pattern/0805/patterns-part1.csv.gz", 
     compression="gzip",
     usecols=["safegraph_place_id",
              "visits_by_day",
@@ -18,39 +18,4 @@ df_pattern = pd.read_csv(
              "date_range_start"])
 df_pattern.head(3)
 # %%
-df_poi = pd.read_csv("data/processed/RMDS_poi.csv",usecols=["safegraph_place_id"])
-df_poi.head(3)
-
-# %%
-df_pattern = pd.merge(df_poi, df_pattern, how="inner")
-
-# %%
-df_pattern.shape
-
-# %%
-df_pattern.info()
-
-# %%
-df_pattern_filter = pd.read_csv(
-    "data/processed/patterns-1.csv", 
-    usecols=["safegraph_place_id",
-             "visits_by_day",
-             "visits_by_each_hour",
-             "median_dwell",
-             "date_range_start",
-             "date_range_end"])
-
-# %%
-df_pattern_filter.head()
-
-# %%
-df_pattern_filter.shape
-
-
-# %%
-df_pattern_filter.info()
-
-# %%
-int("0715") + 7
-
-# %%
+df_pattern.tail(3)
