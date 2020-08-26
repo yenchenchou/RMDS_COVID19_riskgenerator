1d564ea8300bfdcc700bacd95f6dfef5e7a231e9
Yen-Chen Chou <intelchou@gmail.com>
2020-08-26 15:45:59 -0500

    add risk score

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 curl -o data/raw/Covid-19-density.csv https://raw.githubusercontent.com/ANRGUSC/lacounty_covid19_data/master/data/Covid-19-density.csv
 curl -o data/raw/lacounty_covid.json https://raw.githubusercontent.com/ANRGUSC/lacounty_covid19_data/master/data/lacounty_covid.json
 
+mkdir data/external/Core_POI_Folder
 tar -xvf data/external/Core-USA-August2020-Release-CORE_POI-2020_07-2020-08-07.gz -C data/external/Core_POI_Folder/
 
 python src/python/export_data.py 0805 0812 0819
