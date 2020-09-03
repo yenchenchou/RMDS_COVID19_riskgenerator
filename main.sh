87bee2425d857a9aa4cca7e6efc86abb0a9ef511
Yen-Chen Chou <intelchou@gmail.com>
2020-09-03 08:50:50 -0500

    add risk score

--- main.sh
+++ main.sh
@@ -15,13 +15,13 @@ WEEK3=$3
 # move old LA county case/death csv and load the new
 echo "move old test/death data to data/archive if exist..."
 # find files modified more than 48 hours ago
-find data/external -ctime 1 -type f -name "LA_County_Covid19*" -exec mv {} data/archive/ \;
+# find data/external -ctime 1 -type f -name "LA_County_Covid19*" -exec mv {} data/archive/ \;
 mv data/archive/LA_County_Covid19_CSA_testing_table.csv data/archive/LA_County_Covid19_CSA_testing_table_$YESTERDAY.csv
 mv data/archive/LA_County_Covid19_CSA_case_death_table.csv data/archive/LA_County_Covid19_CSA_case_death_table_$YESTERDAY.csv
-echo "rename LA_County_Covid19_CSA_testing_table if exist..."
-mv data/external/LA_County_Covid19_CSA_testing_table\ copy.csv data/external/LA_County_Covid19_CSA_testing_table.csv
-echo "rename LA_County_Covid19_CSA_case_death_table if exist..."
-mv data/external/LA_County_Covid19_CSA_case_death_table\ copy.csv data/external/LA_County_Covid19_CSA_case_death_table.csv
+# echo "rename LA_County_Covid19_CSA_testing_table if exist..."
+# mv data/external/LA_County_Covid19_CSA_testing_table\ copy.csv data/external/LA_County_Covid19_CSA_testing_table.csv
+# echo "rename LA_County_Covid19_CSA_case_death_table if exist..."
+# mv data/external/LA_County_Covid19_CSA_case_death_table\ copy.csv data/external/LA_County_Covid19_CSA_case_death_table.csv
 
 
 # move the old risk score to risk_history
